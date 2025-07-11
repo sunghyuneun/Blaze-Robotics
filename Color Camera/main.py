a133db0a39174799371c4b82c0fd7a4d0893014b
Greg Eun <33554699+CodeBlazing@users.noreply.github.com>
2025-07-11 10:33:26 -0700

    Fixed Webcam Color Analyzer

--- Color Camera/main.py
+++ Color Camera/main.py
@@ -1,11 +1,17 @@
 import cv2
+import numpy
 from PIL import Image
 from util import get_limits
 
 yellow = [0, 255, 255] #yellow in RGB
 blue = [255, 0, 0]
-green = [0, 255, 0]
-red = [0, 0, 255]
+greenLowerLimit = numpy.array([25, 52, 72], dtype=numpy.uint8)
+greenUpperLimit = numpy.array([102, 255, 255], dtype=numpy.uint8)
+redLowerLimit1 = numpy.array([0, 100, 100], dtype=numpy.uint8)
+redUpperLimit1 = numpy.array([10, 255, 255], dtype=numpy.uint8)
+redLowerLimit2 = numpy.array([160, 100, 100], dtype=numpy.uint8)
+redUpperLimit2 = numpy.array([179, 255, 255], dtype=numpy.uint8)
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -20,22 +26,24 @@ while True:
     if blueBbox is not None:
         x1,y1,x2,y2 = blueBbox
         cv2.rectangle(frame, (x1,y1), (x2,y2), (255, 0, 0), 5)
+        cv2.putText(frame, 'BLUE OBJECT',(x1,y2+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0),2,cv2.LINE_AA,False)
+        
 
-    greenLowerLimit, greenUpperLimit = get_limits(colour = green)
     greenMask = cv2.inRange(hsvImage, greenLowerLimit, greenUpperLimit)
     greenMaskPil = Image.fromarray(greenMask)
     greenBbox = greenMaskPil.getbbox()
     if greenBbox is not None:
         x1,y1,x2,y2 = greenBbox
         cv2.rectangle(frame, (x1,y1), (x2,y2), (0, 255, 0), 5)
+        cv2.putText(frame, 'GREEN OBJECT',(x1,y2+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2,cv2.LINE_AA,False)
     
-    redLowerLimit, redUpperLimit = get_limits(colour = red)
-    redMask = cv2.inRange(hsvImage, redLowerLimit, redUpperLimit)
+    redMask = cv2.inRange(hsvImage, redLowerLimit1, redUpperLimit1) + cv2.inRange(hsvImage, redLowerLimit2, redUpperLimit2)
     redMaskPil = Image.fromarray(redMask)
     redBbox = redMaskPil.getbbox()
     if redBbox is not None:
         x1,y1,x2,y2 = redBbox
         cv2.rectangle(frame, (x1,y1), (x2,y2), (0, 0, 255), 5)
+        cv2.putText(frame, 'RED OBJECT',(x1,y2+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA,False)
 
     yellowLowerLimit, yellowUpperLimit = get_limits(colour = yellow)
     yellowMask = cv2.inRange(hsvImage, yellowLowerLimit, yellowUpperLimit)
@@ -44,6 +52,7 @@ while True:
     if yellowBbox is not None:
         x1,y1,x2,y2 = yellowBbox
         cv2.rectangle(frame, (x1,y1), (x2,y2), (0, 255, 255), 5)
+        cv2.putText(frame, 'YELLOW OBJECT',(x1,y2+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255),2,cv2.LINE_AA,False)
     
     cv2.imshow('Frame', frame)
 
