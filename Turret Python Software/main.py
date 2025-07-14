f107f62ef95ee054c4a98399b934580d8b774501
Greg Eun <33554699+CodeBlazing@users.noreply.github.com>
2025-07-13 19:39:10 -0700

    Created the Python file in use for Turret

---
+++ Turret Python Software/main.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy
+from PIL import Image
+
+redLowerLimit1 = numpy.array([0, 120, 120], dtype=numpy.uint8)
+redUpperLimit1 = numpy.array([10, 255, 255], dtype=numpy.uint8)
+redLowerLimit2 = numpy.array([160, 120, 120], dtype=numpy.uint8)
+redUpperLimit2 = numpy.array([179, 255, 255], dtype=numpy.uint8)
+
+cap = cv2.VideoCapture(0)
+kernel = numpy.ones((5,5), numpy.uint8)
+
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+#width = 640
+
+while True:
+    ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    redMask = cv2.inRange(hsvImage, redLowerLimit1, redUpperLimit1) + cv2.inRange(hsvImage, redLowerLimit2, redUpperLimit2)
+    cleanRedMask = cv2.morphologyEx(redMask, cv2.MORPH_OPEN, kernel)
+    cleanRedMask = cv2.morphologyEx(cleanRedMask, cv2.MORPH_CLOSE, kernel)
+    redMaskPil = Image.fromarray(cleanRedMask)
+    redBbox = redMaskPil.getbbox()
+    if redBbox is not None:
+        x1,y1,x2,y2 = redBbox
+        cv2.rectangle(frame, (x1,y1), (x2,y2), (0, 0, 255), 5)
+        centerX = (x1 + x2) / 2
+        if centerX < (width * .45):
+            cv2.putText(frame, 'TARGET: LEFT',(x1,y2+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA,False)
+        elif centerX < (width * .55):
+            cv2.putText(frame, 'TARGET: READY TO FIRE',(x1,y2+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA,False)
+        else:
+            cv2.putText(frame, 'TARGET: RIGHT',(x1,y2+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA,False)
+        
+    
+    cv2.imshow('Frame', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+
+cv2.destroyAllWindows()
+
